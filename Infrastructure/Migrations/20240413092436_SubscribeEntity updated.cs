eaba0061910d1ac065f36d39489e11ffc484d4a3
EmmaK93 <ems.93@hotmail.com>
2024-04-19 10:42:27 +0200

    Add project files.

---
+++ Infrastructure/Migrations/20240413092436_SubscribeEntity updated.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SubscribeEntityupdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AdvertisingUpdate",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "DailyNewsLetter",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EventUpdates",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Podcast",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "StartupWeekly",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WeekinReview",
+                table: "Subscribers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdvertisingUpdate",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "DailyNewsLetter",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "EventUpdates",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "Podcast",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "StartupWeekly",
+                table: "Subscribers");
+
+            migrationBuilder.DropColumn(
+                name: "WeekinReview",
+                table: "Subscribers");
+        }
+    }
+}
